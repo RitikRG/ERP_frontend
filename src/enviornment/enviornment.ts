547b035ab1ae95190e870b71268b14a6bab7aa3d
Ritik Gupta <ritik.mpm@gmail.com>
2026-04-04 15:04:39 +0530

    Live Backend

--- src/enviornment/enviornment.ts
+++ src/enviornment/enviornment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:4000/api', // localhost
-  // apiUrl: 'https://erp-backend-tsrf.onrender.com/api', // live
+  // apiUrl: 'http://localhost:4000/api', // localhost
+  apiUrl: 'https://erp-backend-tsrf.onrender.com/api', // live
   vapidPublicKey:
     'BFJGrJNeLC3H_IgkGXItTQUSRl5vCPS3iZYnMSlvhRj2B1Dztm9B4TqEN4wWWoHw6UH7Bohqf1Sc76WuLaYfnv4',
 };
